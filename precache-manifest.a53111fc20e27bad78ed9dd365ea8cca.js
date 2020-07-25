770323d11753abe34d33b0c8aed0fe3fc1ff70d6
Sweta Purbiya <jaiswal.sweta@gmail.com>
2020-07-25 12:00:42 -0700

    Updates

---
+++ precache-manifest.a53111fc20e27bad78ed9dd365ea8cca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "312ff7694b18d92d72ebfe6489c5592f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3a4ed89f0e7bffc50be2",
+    "url": "/monsters-rolodex/static/css/main.d365a455.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a4ed89f0e7bffc50be2",
+    "url": "/monsters-rolodex/static/js/main.450cf9c0.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
